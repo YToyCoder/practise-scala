e46de9bbcba4fc3a046e6f61cdcf3fc04e6b97a5
forsilence <3312034934@qq.com>
2022-07-29 16:03:57 +0800

    Scala List prepend(+:) & apppend(:+)

---
+++ src/main/scala/practise/UsingList.scala
@@ -0,0 +1,50 @@
+package  practise
+
+class UsingList {
+
+  def weekends() : String = {
+    val days : List[String] = List("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    val when = "AM" :: "PM" :: Nil
+    days match {
+      case firstDay :: otherDays => firstDay
+      case List() => "empty"
+    }
+  }
+
+  def prepend() : Long = {
+
+    def ad[T](ls : List[T], el : T) : List[T] = ls.+:(el)
+    time{
+      listAdd(ad(_, _))
+    }
+
+  }
+
+  def append() : Long = {
+    def add[T](ls : List[T], el : T) : List[T] = ls.:+(el)
+    time{ 
+      listAdd(add(_, _))
+    }
+  }
+
+
+  def listAdd[T](op : (List[String], String) => List[String]): Unit = {
+    var list : List[String] = List()
+    for(i <- 0 to 3000)
+      list = op(list, "")
+  }
+
+  def time(run : => Unit): Long = {
+    val start : Long = System.currentTimeMillis() 
+    run
+    val end : Long = System.currentTimeMillis()
+    end - start
+  }
+
+  def prefix() : Unit = {
+    val pf : List[String] = List("A", "B", "C")
+    val ls : List[String] = List("l", "i" , "s" ,"t")
+    println((ls ::: pf ).mkString)
+  }
+ 
+}
---
+++ src/test/scala/practise/ListSpec.scala
@@ -0,0 +1,31 @@
+package practise
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ListSpec extends AnyFlatSpec with Matchers { 
+
+
+  "List factory method" should "create a List Object" in {
+    val days : List[String] = List("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    days(0) shouldEqual "Sunday"
+    days.length shouldEqual 7
+  }
+
+  "Weekends" should "get first day of weekends" in {
+    new UsingList().weekends() shouldEqual "Sunday"
+  }
+
+  "List prepend" should "faster than append" in {
+    val code = new UsingList()
+    val pLag = code.prepend() 
+    val aLag = code.append()
+    aLag > pLag shouldBe true
+  }
+
+  "List +++" should " nothing to show" in {
+    val code = new UsingList()
+    code.prefix()
+  }
+
+}
