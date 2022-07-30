6e049e3df2e7990e00867e90c9484442e040aca1
forsilence <3312034934@qq.com>
2022-07-30 22:27:14 +0800

    Leetcode add problem description

--- src/main/scala/practise/Leetcode.scala
+++ src/main/scala/practise/Leetcode.scala
@@ -14,6 +14,20 @@ object Leetcode {
   def isPowerOfTwo(n: Int): Boolean = n > 0 && (n & (n - 1)) == 0 
 
 
+  /*
+
+  给你一个由 不同 整数组成的整数数组 arr 和一个整数 k 。
+
+  每回合游戏都在数组的前两个元素（即 arr[0] 和 arr[1] ）之间进行。比较 arr[0] 与 arr[1] 的大小，较大的整数将会取得这一回合的胜利并保留在位置 0 ，较小的整数移至数组的末尾。当一个整数赢得 k 个连续回合时，游戏结束，该整数就是比赛的 赢家 。
+
+  返回赢得比赛的整数。
+
+  题目数据 保证 游戏存在赢家。
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode.cn/problems/find-the-winner-of-an-array-game
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+  */
   def getWinner(arr: Array[Int], k: Int): Int ={
 
     def find(arr: Array[Int], k : Int, count : Int, max : Int, next : Int) : Int = {
