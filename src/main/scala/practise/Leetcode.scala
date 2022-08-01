3351d916870ce9e76f12f1deb4067f88fa7d8594
forsilence <3312034934@qq.com>
2022-08-01 21:23:00 +0800

    Leetcode generateParenthesis

--- src/main/scala/practise/Leetcode.scala
+++ src/main/scala/practise/Leetcode.scala
@@ -1,6 +1,7 @@
 package  practise
 
 import scala.collection.mutable
+import scala.collection.IterableFactory.apply
 
 object Leetcode {
   
@@ -175,5 +176,43 @@ object Leetcode {
     build(grid, 0, 0, grid.length)
   }
 
+
+  //   数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+
+  //  
+
+  //   示例 1：
+
+  //   输入：n = 3
+  //   输出：["((()))","(()())","(())()","()(())","()()()"]
+  //   示例 2：
+
+  //   输入：n = 1
+  //   输出：["()"]
+  //    
+
+  //   提示：
+
+  //   1 <= n <= 8
+
+  //   来源：力扣（LeetCode）
+  //   链接：https://leetcode.cn/problems/generate-parentheses
+  //   著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+  def generateParenthesis(n: Int): List[String] = {
+  
+    def dfs(str : String, left : Int, right :Int, cache : mutable.ListBuffer[String]) : Unit = {
+      if(left == 0 && right == 0)
+        cache.addOne(str)
+      else {
+        if(left <= right){
+          if(left > 0) dfs(str + "(", left - 1, right, cache)
+          if(right > 0) dfs(str + ")", left, right - 1, cache)
+        }
+      }
+    }
+    val ans = mutable.ListBuffer[String]()
+    dfs("", n, n, ans)
+    ans.toList
+  }
 
 }
