efb885e525fb63f2ee7081fd3daf698611af11a8
forsilence <3312034934@qq.com>
2022-08-02 14:56:45 +0800

    Leetcode intersection more faster one

--- src/main/scala/practise/Leetcode.scala
+++ src/main/scala/practise/Leetcode.scala
@@ -214,27 +214,35 @@ object Leetcode {
     ans.toList
   }
 
+
+  // 给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+
+  // 示例 1：
+
+  // 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+  // 输出：[2]
+  // 示例 2：
+
+  // 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+  // 输出：[9,4]
+  // 解释：[4,9] 也是可通过的
+
+  // 来源：力扣（LeetCode）
+  // 链接：https://leetcode.cn/problems/intersection-of-two-arrays
+  // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
   
   def intersection(nums1: Array[Int], nums2: Array[Int]): Array[Int] = {
-    var cache : Set[Int] = Set.empty
-    var ans : Set[Int] = Set.empty
-    util.Sorting.quickSort(nums1)
-    util.Sorting.quickSort(nums2)
-    var preCache : Int = -1
-    for(i <- 0 until nums1.length if preCache != nums1(i)
-    ) {
-      preCache = nums1(i)
-      cache = cache + nums1(i)
+    import scala.collection.mutable.HashSet
+    import scala.collection.mutable.ArrayBuilder
+
+    val builder : ArrayBuilder[Int] = ArrayBuilder.make
+    val recorder : HashSet[Int] = HashSet()
+    for(i <- nums1) recorder.add(i)
+    for(i <- nums2 if recorder.contains(i)){
+      builder.addOne(i)
+      recorder.remove(i)
     }
-    preCache = -1
-    for(i <- 0 until nums2.length if preCache != nums2(i)
-    ){
-      preCache = nums2(i)
-      val old = cache
-      cache = cache + nums2(i)
-      if(old == cache) ans = ans + nums2(i)
-    }
-    ans.toArray
+    builder.result()
   }
 
 }
