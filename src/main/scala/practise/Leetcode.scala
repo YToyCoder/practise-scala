d913e97bdab8be4d29519e31c15baf51d3e7107c
forsilence <3312034934@qq.com>
2022-08-02 18:41:56 +0800

    Leetcode doc

--- src/main/scala/practise/Leetcode.scala
+++ src/main/scala/practise/Leetcode.scala
@@ -263,6 +263,13 @@ object Leetcode {
 
   case class IntHolder(var value : Int);
 
+  // 给定一个包含大写字母和小写字母的字符串 s ，返回 通过这些字母构造成的 最长的回文串 。
+
+  // 在构造过程中，请注意 区分大小写 。比如 "Aa" 不能当做一个回文字符串。
+
+  // 来源：力扣（LeetCode）
+  // 链接：https://leetcode.cn/problems/longest-palindrome
+  // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
   def longestPalindrome(s: String): Int = {
     import scala.collection.mutable.HashMap
 
