b1e27b1380f6faec0f02e6b6881afe70008fbc99
forsilence <3312034934@qq.com>
2022-12-17 10:19:15 +0800

    format code

--- src/main/scala/practise/Leetcode.scala
+++ src/main/scala/practise/Leetcode.scala
@@ -4,10 +4,9 @@ import scala.collection.mutable
 
 object Leetcode {
   
-  /*
+  /**
   给你一个整数 n，请你判断该整数是否是 2 的幂次方。如果是，返回 true ；否则，返回 false 。
-
-  如果存在一个整数 x 使得 n == 2x ，则认为 n 是 2 的幂次方。
+  如果存在一个整数 x 使得n == 2x ，则认为 n 是 2 的幂次方。
 
   来源：力扣（LeetCode）
   链接：https://leetcode.cn/problems/power-of-two
@@ -16,14 +15,10 @@ object Leetcode {
   def isPowerOfTwo(n: Int): Boolean = n > 0 && (n & (n - 1)) == 0 
 
 
-  /*
-
+  /**
   给你一个由 不同 整数组成的整数数组 arr 和一个整数 k 。
-
   每回合游戏都在数组的前两个元素（即 arr[0] 和 arr[1] ）之间进行。比较 arr[0] 与 arr[1] 的大小，较大的整数将会取得这一回合的胜利并保留在位置 0 ，较小的整数移至数组的末尾。当一个整数赢得 k 个连续回合时，游戏结束，该整数就是比赛的 赢家 。
-
   返回赢得比赛的整数。
-
   题目数据 保证 游戏存在赢家。
 
   来源：力扣（LeetCode）
@@ -45,16 +40,13 @@ object Leetcode {
   } 
 
 
-  /*
+  /**
   圣诞活动预热开始啦，汉堡店推出了全新的汉堡套餐。为了避免浪费原料，请你帮他们制定合适的制作计划。
-
-  给你两个整数 tomatoSlices 和 cheeseSlices，分别表示番茄片和奶酪片的数目。不同汉堡的原料搭配如下：
-
+  给你两个整数tomatoSlices和cheeseSlices，分别表示番茄片和奶酪片的数目。不同汉堡的原料搭配如下：
   巨无霸汉堡：4 片番茄和 1 片奶酪
-  小皇堡：2 片番茄和 1 片奶酪
-  请你以 [total_jumbo, total_small]（[巨无霸汉堡总数，小皇堡总数]）的格式返回恰当的制作方案，使得剩下的番茄片 tomatoSlices 和奶酪片 cheeseSlices 的数量都是 0。
-
-  如果无法使剩下的番茄片 tomatoSlices 和奶酪片 cheeseSlices 的数量为 0，就请返回 []。
+  小皇堡：2 片番茄和1 片奶酪
+  请你以total_jumbo, total_small]（[巨无霸汉堡总数，小皇堡总数]）的格式返回恰当的制作方案，使得剩下的番茄片tomatoSlices和奶酪片cheeseSlices的数量都是0。
+  如果无法使剩下的番茄片tomatoSlices和奶酪片cheeseSlices的数量为0，就请返回。
 
   来源：力扣（LeetCode）
   链接：https://leetcode.cn/problems/number-of-burgers-with-no-waste-of-ingredients
@@ -67,14 +59,10 @@ object Leetcode {
   }
 
 
-  /*
-
+  /**
   如果字符串 s 中 不存在 两个不同字符 频次 相同的情况，就称 s 是 优质字符串 。
-
   给你一个字符串 s，返回使 s 成为 优质字符串 需要删除的 最小 字符数。
-
   字符串中字符的 频次 是该字符在字符串中的出现次数。例如，在字符串 "aab" 中，'a' 的频次是 2，而 'b' 的频次是 1 。
-
   来源：力扣（LeetCode）
   链接：https://leetcode.cn/problems/minimum-deletions-to-make-character-frequencies-unique
   著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -100,10 +88,7 @@ object Leetcode {
     ans
   }
 
-  /*
-
-  给定一个字符串 s ，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
-
+  /** 给定一个字符串 s ，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
   */
   def reverseWords(s: String): String = {
     val resultBuilder = mutable.StringBuilder.newBuilder
@@ -122,11 +107,9 @@ object Leetcode {
   }
 
 
-  /*
+  /**
   给你一个整数 n，请你返回一个含 n 个字符的字符串，其中每种字符在该字符串中都恰好出现 奇数次 。
-
   返回的字符串必须只含小写英文字母。如果存在多个满足题目要求的字符串，则返回其中任意一个即可。
-
   来源：力扣（LeetCode）
   链接：https://leetcode.cn/problems/generate-a-string-with-characters-that-have-odd-counts
   著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -176,24 +159,15 @@ object Leetcode {
   }
 
 
-  //   数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
-
-  //  
-
+  //   数字 n代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
   //   示例 1：
-
   //   输入：n = 3
   //   输出：["((()))","(()())","(())()","()(())","()()()"]
   //   示例 2：
-
   //   输入：n = 1
   //   输出：["()"]
-  //    
-
   //   提示：
-
   //   1 <= n <= 8
-
   //   来源：力扣（LeetCode）
   //   链接：https://leetcode.cn/problems/generate-parentheses
   //   著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -215,14 +189,11 @@ object Leetcode {
   }
 
 
-  // 给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
-
+  // 给定两个数组nums1和nums2 ，返回 它们的交集。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
   // 示例 1：
-
   // 输入：nums1 = [1,2,2,1], nums2 = [2,2]
   // 输出：[2]
   // 示例 2：
-
   // 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
   // 输出：[9,4]
   // 解释：[4,9] 也是可通过的
@@ -247,10 +218,7 @@ object Leetcode {
 
 
   // 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
-
   // 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
-
-  //  
 
   // 来源：力扣（LeetCode）
   // 链接：https://leetcode.cn/problems/power-of-four
@@ -263,9 +231,8 @@ object Leetcode {
 
   case class IntHolder(var value : Int);
 
-  // 给定一个包含大写字母和小写字母的字符串 s ，返回 通过这些字母构造成的 最长的回文串 。
-
-  // 在构造过程中，请注意 区分大小写 。比如 "Aa" 不能当做一个回文字符串。
+  // 给定一个包含大写字母和小写字母的字符串s，返回通过这些字母构造成的 最长的回文串。
+  // 在构造过程中，请注意 区分大小写 。比如Aa"不能当做一个回文字符串。
 
   // 来源：力扣（LeetCode）
   // 链接：https://leetcode.cn/problems/longest-palindrome
